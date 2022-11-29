178008b63d9997b39c93e55275ddf1abee30fe7f
Yuri Han <45699207+urihan@users.noreply.github.com>
2022-11-29 23:08:11 +0900

    Change in background

---
+++ sketch.js
@@ -0,0 +1,26 @@
+// https://editor.p5js.org/hafiyyandi/sketches/H1ml1wZFm
+
+var dAmt = 100;
+
+function setup() {
+  createCanvas(1920, 1080);
+  background(0);
+  noStroke();
+
+  fill('white');
+  for (var i = 0; i < dAmt; i++){
+    var al = random (30, 255);
+    var r_col = random (150,255);
+    var g_col = random (150, 255);
+  	var y = random(0, height);
+    var x = random(0, width);
+    var r = random(3,10);
+    fill(r_col, g_col, 0, al);
+    ellipse(x, y, r, r);
+  }
+
+}
+
+function draw() {
+
+}
