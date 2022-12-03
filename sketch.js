4794f8ab480d723f028e29bc64b2ca0d8302850e
Yuri Han <45699207+urihan@users.noreply.github.com>
2022-12-03 20:14:16 +0900

    update

--- sketch.js
+++ sketch.js
@@ -3,15 +3,15 @@
 var dAmt = 100;
 
 function setup() {
-  createCanvas(1440, 1000);
+  createCanvas(1440, 815);
   background(0);
   noStroke();
 
   fill('white');
   for (var i = 0; i < dAmt; i++){
     var al = random (30, 255);
-    var r_col = random (150,255);
-    var g_col = random (150, 255);
+    var r_col = random (10,255);
+    var g_col = random (10, 255);
   	var y = random(0, height);
     var x = random(0, width);
     var r = random(3,10);
